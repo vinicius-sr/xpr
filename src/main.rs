6761eb290001053f98110f9c83bde491700bd336
vinicius.ramos <vinicius.silva.ramos01@gmail.com>
2026-08-14 20:41:00 -0300

    rewrite scanner with next/ensure, add unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,37 @@
 use std::str::Chars;
 
+use self::ExpressionError::{InvalidChar, InvalidTerm, UnexpectedEof};
+
 fn main() {
-    let source =
-        "pdiv(81.34,x[:, 0]*pdiv(x[:, 3],psqrt(np.exp(np.sin(x[:, 0]))*x[:, 3])))*np.sin(88.10)";
+    let source = "pdiv(81.34,x(0)*pdiv(x(3),psqrt(np.exp(np.sin(x(0)))*x(3))))*np.sin(88.10)";
 
     let mut scanner = Scanner::new(source);
-    scanner.scan();
+    println!("{:?}", scanner.expr())
+}
+
+#[derive(Debug)]
+enum ExpressionError {
+    InvalidChar {
+        current: char,
+        expected: char,
+        buffer: String,
+    },
+    UnexpectedEof,
+    InvalidTerm(String),
+}
+
+#[derive(Debug, PartialEq)]
+enum OpCode {
+    Pdiv,
+    Read(u32),
 }
 
 struct Scanner<'a> {
     source: Chars<'a>,
     buffer: String,
+    instructions: Vec<OpCode>,
+    stack: Vec<f64>,
+    c: char,
 }
 
 impl<'a> Scanner<'a> {
@@ -18,34 +39,182 @@ impl<'a> Scanner<'a> {
         Self {
             source: source.chars(),
             buffer: String::new(),
+            instructions: Vec::new(),
+            stack: Vec::new(),
+            c: char::MIN,
         }
     }
 
-    fn scan(&mut self) {
+    pub fn expr(&mut self) -> Result<(), ExpressionError> {
+        if self.next() {
+            if self.is_alpha() {
+                self.fill_buffer();
+            }
+            // println!("buffer: '{}' - current: '{}'", self.buffer, self.c);
+
+            if let Err(e) = match self.buffer.as_str() {
+                "pdiv" => {
+                    self.instructions.push(OpCode::Pdiv);
+                    self.buffer.clear();
+                    self.ensure('(')
+                        .and_then(|_| self.expr())
+                        .and_then(|_| self.ensure(','))
+                        .and_then(|_| self.expr())
+                        .and_then(|_| self.ensure(')'))
+                }
+                // "x" => {
+                //     self.instructions.push(OpCode::Read);
+                //     self.ensure( '(')
+                //         .and_then(|_| self.expr())
+                //         .and_then(|_| self.ensure( ')'))
+                // }
+                e => match e.parse::<f64>() {
+                    Ok(v) => {
+                        self.stack.push(v);
+                        self.buffer.clear();
+                        Ok(())
+                    }
+                    Err(_) => Err(InvalidTerm(self.buffer.clone())),
+                },
+            } {
+                return Err(e);
+            }
+            Ok(())
+        } else {
+            Err(UnexpectedEof)
+        }
+
+        // while let Some(c) = self.source.next() {
+        //     if c.is_whitespace() {
+        //         continue;
+        //     }
+
+        //     // can be func name,
+        //     if c.is_ascii_lowercase()
+        //         || c.is_ascii_uppercase()
+        //         || c.is_ascii_digit()
+        //         || c == '.'
+        //         || c == '_'
+        //     {
+        //         self.buffer.push(c);
+        //         continue;
+        //     }
+
+        //     println!("{}", self.buffer);
+        //     println!("{}", c);
+
+        //     if let Err(e) = match self.buffer.as_str() {
+        //         "pdiv" => {
+        //             self.instructions.push(OpCode::Pdiv);
+        //             self.ensure(c, '(')
+        //                 .and_then(|_| self.expr())
+        //                 .and_then(|_| self.ensure(c, ','))
+        //                 .and_then(|_| self.expr())
+        //                 .and_then(|_| self.ensure(c, ')'))
+        //         }
+        //         "x" => {
+        //             self.instructions.push(OpCode::Read);
+        //             self.ensure(c, '(')
+        //                 .and_then(|_| self.expr())
+        //                 .and_then(|_| self.ensure(c, ')'))
+        //         }
+        //         e => match e.parse::<f64>() {
+        //             Ok(v) => {
+        //                 self.stack.push(v);
+        //                 Ok(())
+        //             }
+        //             Err(_) => Err(InvalidTerm(self.buffer.clone())),
+        //         },
+        //     } {
+        //         return Err(e);
+        //     }
+
+        //     self.buffer.clear();
+        // }
+    }
+
+    fn fill_buffer(&mut self) {
+        self.buffer.push(self.c);
+        while self.next() && self.is_alpha() {
+            self.buffer.push(self.c);
+        }
+    }
+
+    fn is_alpha(&self) -> bool {
+        self.c.is_ascii_lowercase()
+            || self.c.is_ascii_uppercase()
+            || self.c.is_ascii_digit()
+            || self.c == '.'
+            || self.c == '_'
+    }
+
+    fn next(&mut self) -> bool {
         while let Some(c) = self.source.next() {
             if c.is_whitespace() {
                 continue;
             }
 
-            if c.is_ascii_lowercase() || c.is_ascii_uppercase() {
-                self.buffer.push(c);
-                continue;
-            }
+            self.c = c;
+            return true;
+        }
 
-            match self.buffer.as_str() {
-                "pdiv" => {
-                    let e = self.source.next();
-                }
-                _ => todo!("Unexpected function: {}", self.buffer),
-            }
+        return false;
+    }
 
-            todo!("Unexpected char: '{}', current buffer: {}", c, self.buffer)
+    fn ensure(&mut self, expected: char) -> Result<(), ExpressionError> {
+        if self.c == expected {
+            // println!("Ensure '{}' - GOOD", expected);
+            Ok(())
+        } else {
+            Err(InvalidChar {
+                current: self.c,
+                expected,
+                buffer: self.buffer.to_string(),
+            })
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{ExpressionError::UnexpectedEof, OpCode};
+
+    use super::{ExpressionError, Scanner};
+
+    #[test]
+    fn test_empty_source_code() {
+        let mut scanner = Scanner::new("");
+        assert!(matches!(scanner.expr(), Err(UnexpectedEof)));
+    }
+
+    #[test]
+    fn test_whitespace_source_code() {
+        let mut scanner = Scanner::new("   \n\n\t");
+        assert!(matches!(scanner.expr(), Err(UnexpectedEof)));
+    }
+
+    #[test]
+    fn test_div_simple() {
+        let mut scanner = Scanner::new("pdiv(12, 3)");
+
+        match scanner.expr() {
+            Ok(()) => {
+                assert_eq!(scanner.instructions, vec![OpCode::Pdiv]);
+                assert_eq!(scanner.stack, vec![12., 3.]);
+            }
+            Err(e) => panic!("{:?}", e),
         }
     }
 
-    // fn ensure(&mut self, expected: char) -> bool {
-    //     if let Some(current) = self.source.next() && current == expected{
-            
-    //     }
+    // #[test]
+    // fn test_invalid_char() {
+    //     let mut scanner = Scanner::new("pdiv 12, 3)");
+    //     assert!(matches!(scanner.expr(), Err(ExpressionError::InvalidChar { .. })));
+    // }
+
+    // #[test]
+    // fn test_invalid_term() {
+    //     let mut scanner = Scanner::new("abc");
+    //     assert!(matches!(scanner.expr(), Err(ExpressionError::InvalidTerm(_))));
     // }
 }
